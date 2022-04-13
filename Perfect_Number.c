b97d68ea0209fa8239f57cc1d817590c1444724b
Alekhya58 <97424622+Alekhya58@users.noreply.github.com>
2022-04-13 12:08:26 +0530

    #include<stdio.h> int main() {     int num,rem,i,sum=0;     scanf("%d",&num);     for(i=1;i<=(num-1);i++)     {         rem = num % i;         if(rem == 0)         {             sum = sum + i;         }
        }
        if(sum == num)
        {
            printf("True");
        }
        else
        {
            printf("False");
        }
        return 0;
    }

---
+++ Perfect_Number.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int num,rem,i,sum=0;
+    scanf("%d",&num);
+    for(i=1;i<=(num-1);i++)
+    {
+        rem = num % i;
+        if(rem == 0)
+        {
+            sum = sum + i;
+        }
+        
+    }
+    if(sum == num)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+    return 0;
+}
