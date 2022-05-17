97085952ad1070534ae92a75bb8095ba23f1c59b
Alekhya58 <97424622+Alekhya58@users.noreply.github.com>
2022-05-17 09:45:59 +0530

    #include<stdio.h> int main() {     int i,n,j;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=n;j++)         {             if(i==j || j==1 || i==n)             {                 printf("*");             }             else             {                 printf(" ");             }         }         printf(" ");
        }
    }

---
+++ print_the_following_pattern.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,j;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            if(i==j || j==1 || i==n)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("
+");
+        
+    }
+}
