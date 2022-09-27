51829b4b18bc1a94a217f15432822b6818b7085c
Alekhya58 <97424622+Alekhya58@users.noreply.github.com>
2022-09-27 20:43:24 +0530

    #include<iostream> #include<math.h> #include<iomanip> using namespace std; int main() {     int p,r,t;     double ci;     cin>>p>>r>>t;     ci=pow((1+r/100.00),t);     cout<<setprecision(2)<<fixed;     cout<<p*ci; }

--- Compound_interest.c
+++ Compound_interest.c
@@ -1,10 +1,13 @@
-#include<stdio.h>
+#include<iostream>
 #include<math.h>
+#include<iomanip>
+using namespace std;
 int main()
 {
-    double p,r,t;
-    scanf("%lf%lf%lf",&p,&r,&t);
-    double ci=p*1.0*pow(1+(r/100.0),t);
-    printf("%.2lf",ci);
-    return 0;
+    int p,r,t;
+    double ci;
+    cin>>p>>r>>t;
+    ci=pow((1+r/100.00),t);
+    cout<<setprecision(2)<<fixed;
+    cout<<p*ci;
 }
